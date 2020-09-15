9360a1a34cc8b4e38cc9dcecf6edcf0a79caa42a
Guido <guidomontorfano78@gmail.com>
2020-09-15 20:41:03 -0300

    add mock test

--- backend-desapp-api/src/test/java/model/DonationTest.java
+++ backend-desapp-api/src/test/java/model/DonationTest.java
@@ -1,10 +1,13 @@
 package model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import antlr.collections.List;
 import ar.edu.unq.desapp.grupoG.backenddesappapi.model.Donation;
 import ar.edu.unq.desapp.grupoG.backenddesappapi.model.Location;
 
@@ -15,6 +18,19 @@ public class DonationTest {
     void setUp() {
         donation = new Donation(0, 1, 2, 5000.45);
     }
+    
+	@Test //EXAMPLE MOCK
+	void contextLoads() {
+		// mock creation
+		List mockedList = mock(List.class);
+//		mockedList.add("one");
+//		mockedList.add("hola");
+		when(mockedList.elementAt(0)).thenReturn("one");
+		when(mockedList.elementAt(1)).thenReturn("hola");
+		
+		assertEquals("hola", mockedList.elementAt(1));
+	
+	}
 
     @Test
     void getIdDonationTest(){
