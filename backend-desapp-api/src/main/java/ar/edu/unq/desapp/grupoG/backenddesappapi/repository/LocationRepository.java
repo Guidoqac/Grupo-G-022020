19aa20aea732cf0d982570e769150572291ac807
Guido <guidomontorfano78@gmail.com>
2020-10-14 18:30:25 -0300

    add services and repositories donation and location

---
+++ backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoG/backenddesappapi/repository/LocationRepository.java
@@ -0,0 +1,16 @@
+package ar.edu.unq.desapp.grupoG.backenddesappapi.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ar.edu.unq.desapp.grupoG.backenddesappapi.model.Location;
+
+public interface LocationRepository extends CrudRepository<Location, Integer> {
+
+    Optional<Location> findById(Integer id);
+
+    List<Location> findAll();
+	
+}
--- backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoG/backenddesappapi/service/DonationService.java
+++ backend-desapp-api/src/main/java/ar/edu/unq/desapp/grupoG/backenddesappapi/service/DonationService.java
@@ -1,25 +1,25 @@
 package ar.edu.unq.desapp.grupoG.backenddesappapi.service;
 
-import ar.edu.unq.desapp.grupoG.backenddesappapi.model.User;
-import ar.edu.unq.desapp.grupoG.backenddesappapi.repository.UserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
-public class UserService {
+import ar.edu.unq.desapp.grupoG.backenddesappapi.model.Donation;
+import ar.edu.unq.desapp.grupoG.backenddesappapi.repository.DonationRepository;
 
+@Service
+public class DonationService {
+	
     @Autowired
-    private UserRepository repository;
+    private DonationRepository repository;
 
     @Transactional
-    public User save(User model) {
+    public Donation save(Donation model) {
         return this.repository.save(model);
     }
 
     @Transactional
-    public User findById(Integer id){
+    public Donation findById(Integer id){
         return this.repository.findById(id).get();
     }
 
