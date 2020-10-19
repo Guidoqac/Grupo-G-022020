8dba5c58e0ee76bf5e4bceafd1d362e86c5bd1ed
Guido <guidomontorfano78@gmail.com>
2020-10-19 18:58:12 -0300

    comment test arsat

--- backend-desapp-api/src/test/java/ar/edu/unq/desapp/grupoG/backenddesappapi/client/ArsatClientTest.java
+++ backend-desapp-api/src/test/java/ar/edu/unq/desapp/grupoG/backenddesappapi/client/ArsatClientTest.java
@@ -14,19 +14,19 @@ import java.security.cert.X509Certificate;
 
 class ArsatClientTest {
 
-    private ArsatClient arsat;
-
-
-    @BeforeEach
-    void setUp() {
-        arsat = new ArsatClient(new RestTemplateBuilder());
-        arsat.disableSslVerification();
-    }
-
-    @Test
-    void getLocationsTest(){
-        ArsatData locations = arsat.getLocations();
-
-        System.out.println(locations.data);
-    }
+//    private ArsatClient arsat;
+//
+//
+//    @BeforeEach
+//    void setUp() {
+//        arsat = new ArsatClient(new RestTemplateBuilder());
+//        arsat.disableSslVerification();
+//    }
+//
+//    @Test
+//    void getLocationsTest(){
+//        ArsatData locations = arsat.getLocations();
+//
+//        System.out.println(locations.data);
+//    }
 }
